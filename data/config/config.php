a3fb1533b0926a19dff8f7bc1762e869e82bbd9d
Bang Ngoc Vu <bangvn.dng@gmail.com>
2014-03-25 09:52:23 +0700

    init

---
+++ data/config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('ECCUBE_INSTALL', 'ON');
+define('HTTP_URL', 'http://local.ec-cube.com/');
+define('HTTPS_URL', 'http://local.ec-cube.com/');
+define('ROOT_URLPATH', '/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'mysql');
+define('DB_USER', 'ec-cube-user');
+define('DB_PASSWORD', 'abcd1234');
+define('DB_SERVER', '127.0.0.1');
+define('DB_NAME', 'ec-cube');
+define('DB_PORT', '3306');
+define('ADMIN_DIR', 'admin/');
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', 'claelaenipaicrouwroumouthedoudokaepiathu');
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', 'mail');
+define('SMTP_HOST', '');
+define('SMTP_PORT', '');
+define('SMTP_USER', '');
+define('SMTP_PASSWORD', '');
